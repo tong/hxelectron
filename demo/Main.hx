7bab5189d2c29cfae6252a64846ad0c7ade170c8
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-20 22:29:30 -0600

    added demo

---
+++ demo/Main.hx
@@ -0,0 +1,21 @@
+import as.*;
+import js.Node;
+
+class Main {
+	static function main() {
+		CrashReporter.start();
+		App.on("window-all-closed", function() {
+			if (Node.process.platform != 'darwin')
+    		App.quit();
+		});
+
+		var mainWindow = null;
+		App.on("ready", function() {
+			mainWindow = new BrowserWindow({width: 800, height: 600});
+			mainWindow.loadUrl('file://' + Node.__dirname + '/index.html');
+			mainWindow.on('closed', function() {
+				mainWindow = null;
+			});
+		});
+	}
+}
