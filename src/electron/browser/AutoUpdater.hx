d372c558d772b945d1dab8d2d16aeca189afac06
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-23 17:38:59 -0600

    added references for AutoUpdater and ContentTracing

---
+++ src/electron/browser/AutoUpdater.hx
@@ -0,0 +1,8 @@
+package electron.browser;
+
+@:jsRequire("auto-updater")
+extern class AutoUpdater {
+  //autoUpdater.setFeedUrl(url)
+  //autoUpdater.checkForUpdates()
+  //https://github.com/atom/electron/blob/master/docs/api/auto-updater.md
+}
---
+++ src/electron/browser/ContentTracing.hx
@@ -0,0 +1,14 @@
+package electron.browser;
+
+@:jsRequire("content-tracing")
+extern class ContentTracing {
+  //tracing.getCategories(callback)
+  //tracing.startRecording(categoryFilter, options, callback)
+  //tracing.stopRecording(resultFilePath, callback)
+  //tracing.startMonitoring(categoryFilter, options, callback)
+  //tracing.stopMonitoring(callback);
+  //tracing.captureMonitoringSnapshot(resultFilePath, callback)
+  //tracing.getTraceBufferUsage(callback)
+  //tracing.setWatchEvent(categoryName, eventName, callback)
+  //tracing.cancelWatchEvent()
+}
