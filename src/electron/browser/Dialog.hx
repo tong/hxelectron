62e99b454568140f97439144f9335117271c874d
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-23 17:40:24 -0600

    added Dialog and GlobalShortcut

---
+++ src/electron/browser/Dialog.hx
@@ -0,0 +1,9 @@
+package electron.browser;
+
+@:jsRequire("dialog")
+extern class Dialog {
+  //dialog.showOpenDialog([browserWindow], [options], [callback])
+  //dialog.showSaveDialog([browserWindow], [options], [callback])
+  //dialog.showMessageBox([browserWindow], options, [callback])
+  //dialog.showErrorBox(title, content)
+}
---
+++ src/electron/browser/GlobalShortcut.hx
@@ -0,0 +1,9 @@
+package electron.browser;
+
+@:jsRequire("global-shortcut")
+extern class GlobalShortcut {
+  //globalShortcut.register(accelerator, callback)
+  //globalShortcut.isRegistered(accelerator)
+  //globalShortcut.unregister(accelerator)
+  //globalShortcut.unregisterAll()
+}
