4e57d6129bb7fed8cef439dfe46f0170e2848c01
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-24 21:13:45 -0600

    added signatures for WebFrame

--- src/electron/renderer/WebFrame.hx
+++ src/electron/renderer/WebFrame.hx
@@ -2,10 +2,14 @@ package electron.renderer;
 
 @:jsRequire("web-frame")
 extern class WebFrame {
-  // webFrame.setZoomFactor(factor)
-  // webFrame.getZoomFactor()
-  // webFrame.setZoomLevel(level)
-  // webFrame.getZoomLevel()
-  // webFrame.setSpellCheckProvider(language, autoCorrectWord, provider)
-  // webFrame.registerUrlSchemeAsSecure(scheme)
+  static function setZoomFactor(factor : Float) : Void;
+  static function getZoomFactor() : Float;
+  static function setZoomLevel(level : Float) : Void;
+  static function getZoomLevel() : Float;
+  static function setSpellCheckProvider(language : String, autoCorrectWord : Bool, provider : Spellcheck) : Void;
+  static function registerUrlSchemeAsSecure(scheme : String) : Void;
 }
+
+typedef Spellcheck = {
+  spellcheck : String -> String
+};
