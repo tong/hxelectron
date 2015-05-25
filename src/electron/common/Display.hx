02a27494664b087c6f0354f2edfd3c2bb5eb8d83
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-24 20:59:00 -0600

    added signatures for Screen

---
+++ src/electron/common/Display.hx
@@ -0,0 +1,8 @@
+package electron.common;
+
+extern class Display {
+  var workAreaSize : { width : Int, height : Int };
+  var bounds : { x : Int, y : Int };
+  //scaleFactor
+  //rotation
+}
--- src/electron/common/Screen.hx
+++ src/electron/common/Screen.hx
@@ -1,10 +1,12 @@
 package electron.common;
 
+import js.node.events.EventEmitter;
+
 @:jsRequire("screen")
-extern class Screen {
-  // screen.getCursorScreenPoint()
-  // screen.getPrimaryDisplay()
-  // screen.getAllDisplays()
-  // screen.getDisplayNearestPoint(point)
-  // screen.getDisplayMatching(rect)
+extern class Screen extends EventEmitter<Screen> {
+  static function getCursorScreenPoint() : { x : Int, y : Int}; // TODO check return type
+  static function getPrimaryDisplay() : Display;
+  static function getAllDisplays() : Array<Display>;
+  static function getDisplayNearestPoint(point : { x : Int, y : Int}) : Display;
+  static function getDisplayMatching(rect : { x : Int, y : Int, width : Int, height : Int}) : Display;
 }
