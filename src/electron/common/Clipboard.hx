52e625420de32682e64c5081cf37217e6e7719af
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-24 20:58:21 -0600

    added signatures for Clipboard

--- src/electron/common/Clipboard.hx
+++ src/electron/common/Clipboard.hx
@@ -2,11 +2,11 @@ package electron.common;
 
 @:jsRequire("clipboard")
 extern class Clipboard {
-  // clipboard.readText([type])
-  // clipboard.writeText(text[, type])
-  // clipboard.readImage([type])
-  // clipboard.writeImage(image[, type])
-  // clipboard.clear([type])
-  // clipboard.has(format[, type])
-  // clipboard.read(format[, type])
+  static function readText(?type : String) : String;
+  static function writeText(text : String, ?type : String) : Void;
+  static function readImage(?type : String) : NativeImage;
+  static function writeImage(image : NativeImage, ?type : String) : Void;
+  static function clear(?type : String) : Void;
+  static function has(format : String, ?type : String) : Bool;
+  static function read(format : String, ?type : String) : Dynamic;
 }
