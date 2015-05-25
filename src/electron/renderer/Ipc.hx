45c257829104897f438ee084ecfc44e3ead1d165
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-24 21:13:28 -0600

    added signatures for Ipc

--- src/electron/renderer/Ipc.hx
+++ src/electron/renderer/Ipc.hx
@@ -1,8 +1,11 @@
 package electron.renderer;
 
+import haxe.extern.Rest;
+import js.node.events.EventEmitter;
+
 @:jsRequire("ipc")
-extern class Ipc {
-  // ipc.send(channel[, args...])
-  // ipc.sendSync(channel[, args...])
-  // ipc.sendToHost(channel[, args...])
+extern class Ipc extends EventEmitter<Ipc> {
+  static function send(channel : String, args : Rest<Dynamic>) : Void;
+  static function sendSync(channel : String, args : Rest<Dynamic>) : Void;
+  static function sendToHost(channel : String, args : Rest<Dynamic>) : Void;
 }
