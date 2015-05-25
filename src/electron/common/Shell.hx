26abf48505a89ebd4c9cd6cbb08245d5f81286fb
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-24 20:58:43 -0600

    added signatures for Shell

--- src/electron/common/Shell.hx
+++ src/electron/common/Shell.hx
@@ -2,9 +2,9 @@ package electron.common;
 
 @:jsRequire("shell")
 extern class Shell {
-  // shell.showItemInFolder(fullPath)
-  // shell.openItem(fullPath)
-  // shell.openExternal(url)
-  // shell.moveItemToTrash(fullPath)
-  // shell.beep()
+  static function showItemInFolder(fullPath : String) : Void;
+  static function openItem(fullPath : String) : Void;
+  static function openExternal(url : String) : Void;
+  static function moveItemToTrash(fullPath : String) : Bool;
+  static function beep() : Void;
 }
