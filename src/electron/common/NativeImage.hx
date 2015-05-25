573a76a3cb9458dfcb84a26a1f4995e853a6fdd3
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-24 20:58:32 -0600

    added signatures for NativeImage

--- src/electron/common/NativeImage.hx
+++ src/electron/common/NativeImage.hx
@@ -1,15 +1,17 @@
 package electron.common;
 
+import js.node.Buffer;
+
 @:jsRequire("native-image")
 extern class NativeImage {
-  // nativeImage.createEmpty()
-  // nativeImage.createFromPath(path)
-  // nativeImage.createFromBuffer(buffer[, scaleFactor])
-  // nativeImage.createFromDataUrl(dataUrl)
-  // NativeImage.toPng()
-  // NativeImage.toJpeg(quality)
-  // NativeImage.toDataUrl()
-  // NativeImage.isEmpty()
-  // NativeImage.getSize()
-  // NativeImage.setTemplateImage(option)
+  static function createEmpty() : NativeImage;
+  static function createFromPath(path : String) : NativeImage;
+  static function createFromBuffer(buffer : Buffer, ?scaleFactor : Float) : NativeImage;
+  static function createFromDataUrl(dataUrl : String) : NativeImage;
+  function toPng() : Buffer;
+  function toJpeg(quality : Int) : Buffer;
+  function toDataUrl() : String;
+  function isEmpty() : Bool;
+  function getSize() : { width : Int, height : Int }; // TODO check return type
+  function setTemplateImage(option : Bool) : Void;
 }
