cec2f4d05e65ff54bbe54e0a30b13e92661d51c5
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-27 22:11:11 -0600

    added openDevTools

--- src/electron/main/BrowserWindow.hx
+++ src/electron/main/BrowserWindow.hx
@@ -4,5 +4,6 @@ package electron.main;
 extern class BrowserWindow {
 	function new(options : BrowserWindowOptions) : Void;
 	function on(eventType : String, callback : Void -> Void) : Void;
+	function openDevTools(?options : { ?detach : Bool }) : Void;
 	function loadUrl(path : String) : Void;
 }
