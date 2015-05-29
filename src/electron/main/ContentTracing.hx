15951626295a7f5d705baf45822cae6221847b3b
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-28 20:35:55 -0600

    ContentTracing members

--- src/electron/main/ContentTracing.hx
+++ src/electron/main/ContentTracing.hx
@@ -2,13 +2,13 @@ package electron.main;
 
 @:jsRequire("content-tracing")
 extern class ContentTracing {
-  //tracing.getCategories(callback)
-  //tracing.startRecording(categoryFilter, options, callback)
-  //tracing.stopRecording(resultFilePath, callback)
-  //tracing.startMonitoring(categoryFilter, options, callback)
-  //tracing.stopMonitoring(callback);
-  //tracing.captureMonitoringSnapshot(resultFilePath, callback)
-  //tracing.getTraceBufferUsage(callback)
-  //tracing.setWatchEvent(categoryName, eventName, callback)
-  //tracing.cancelWatchEvent()
+  function getCategories(callback : Array<String> -> Void) : Void;
+  function startRecording(categoryFilter : String, options : Int, callback : Void -> Void) : Void;
+  function stopRecording(resultFilePath : String, callback : String -> Void) : Void;
+  function startMonitoring(categoryFilter : String, options : Int, callback : Void -> Void) : Void;
+  function stopMonitoring(callback : Void -> Void) : Void;
+  function captureMonitoringSnapshot(resultFilePath : String, callback : Void -> Void) : Void;
+  function getTraceBufferUsage(callback : Void -> Void) : Void;
+  function setWatchEvent(categoryName : String, eventName : String, callback : Void -> Void) : Void;
+  function cancelWatchEvent() : Void;
 }
