11728a0edad13ffd5ae819ba6c1f16c38971e588
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-28 20:35:39 -0600

    AutoUpdater members

--- src/electron/main/AutoUpdater.hx
+++ src/electron/main/AutoUpdater.hx
@@ -2,7 +2,8 @@ package electron.main;
 
 @:jsRequire("auto-updater")
 extern class AutoUpdater {
-  //autoUpdater.setFeedUrl(url)
-  //autoUpdater.checkForUpdates()
+  function setFeedUrl(url : String) : Void;
+  function checkForUpdates() : Void;
+  // TODO add events
   //https://github.com/atom/electron/blob/master/docs/api/auto-updater.md
 }
