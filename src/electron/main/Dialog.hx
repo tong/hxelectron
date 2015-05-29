1b501e733f58b7263aadffe073ca5c3156ad1a07
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-28 20:36:04 -0600

    Dialog members

--- src/electron/main/Dialog.hx
+++ src/electron/main/Dialog.hx
@@ -1,9 +1,33 @@
 package electron.main;
 
+import electron.common.NativeImage;
+
 @:jsRequire("dialog")
 extern class Dialog {
-  //dialog.showOpenDialog([browserWindow], [options], [callback])
-  //dialog.showSaveDialog([browserWindow], [options], [callback])
-  //dialog.showMessageBox([browserWindow], options, [callback])
-  //dialog.showErrorBox(title, content)
+  function showOpenDialog(?browserWindow : BrowserWindow, ?options : OpenDialogOptions, ?callback : Array<String> -> Void) : Void;
+  function showSaveDialog(?browserWindow : BrowserWindow, ?options : SaveDialogOptions, ?callback : String -> Void) : Void;
+  function showMessageBox(?browserWindow : BrowserWindow, options : MessageBoxOptions, ?callback : Int -> Void) : Void;
+  function showErrorBox(title : String, content : String) : Void;
+}
+
+typedef OpenDialogOptions = {
+  title : String,
+  defaultPath : String,
+  filters : Array<{ name : String, extensions : Array<String>}>,
+  properties : Array<String>, // openFile, openDirectory, multiSelections and createDirectory
+}
+
+typedef SaveDialogOptions = {
+  title : String,
+  defaultPath : String,
+  filters : Array<{ name : String, extensions : Array<String>}>
+}
+
+typedef MessageBoxOptions = {
+  type : String, // "none", "info" or "warning"
+  buttons : Array<String>,
+  title : String,
+  message : String,
+  detail : String,
+  icon : NativeImage
 }
