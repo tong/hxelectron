2d4f1600a0d0764d9095b45cc7a3c684b666082e
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-20 20:23:07 -0600

    minor

--- src/electron/main/PowerMonitor.hx
+++ src/electron/main/PowerMonitor.hx
@@ -4,13 +4,13 @@ import js.node.events.EventEmitter;
 
 @:jsRequire("protocol")
 extern class Protocol {
+  // Class: protocol.RequestFileJob(path)
+  // Class: protocol.RequestStringJob(options)
+  // Class: protocol.RequestBufferJob(options)
+  // Class: protocol.RequestErrorJob(code)
   // protocol.registerProtocol(scheme, handler)
   // protocol.unregisterProtocol(scheme)
   // protocol.isHandledProtocol(scheme)
   // protocol.interceptProtocol(scheme, handler)
   // protocol.uninterceptProtocol(scheme)
-  // Class: protocol.RequestFileJob(path)
-  // Class: protocol.RequestStringJob(options)
-  // Class: protocol.RequestBufferJob(options)
-  // Class: protocol.RequestErrorJob(code)
 }
