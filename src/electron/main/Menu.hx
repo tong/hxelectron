04f7bbbfc8863e7b9f54399b6c50de6e80a726ba
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-20 20:00:04 -0600

    added some features to Menu

--- src/electron/main/Menu.hx
+++ src/electron/main/Menu.hx
@@ -2,12 +2,12 @@ package electron.main;
 
 @:jsRequire("menu") // require("remote").require("menu")
 extern class Menu {
+  static function setApplicationMenu(menu : Menu) : Void;
+  static function buildFromTemplate(template : Array<Dynamic>) : Menu;
   function new() : Void;
-  // Class Method: Menu.setApplicationMenu(menu)
-  // Class Method: Menu.setApplicationMenu(menu)
+  function append(menuItem : MenuItem) : Void;
   // Class Method: Menu.buildFromTemplate(template)
   // Menu.popup(browserWindow, [x, y])
-  // Menu.append(menuItem)
   // Menu.insert(pos, menuItem)
   // Menu.items
 }
