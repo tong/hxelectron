bec2436856048a5368f89bb0f050dc030b3e6bc0
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-20 20:13:38 -0600

    added more Menu definitions

--- src/electron/main/Menu.hx
+++ src/electron/main/Menu.hx
@@ -1,13 +1,13 @@
 package electron.main;
 
-@:jsRequire("menu") // require("remote").require("menu")
+// require("remote").require("menu")
+@:jsRequire("menu")
 extern class Menu {
   static function setApplicationMenu(menu : Menu) : Void;
   static function buildFromTemplate(template : Array<Dynamic>) : Menu;
   function new() : Void;
   function append(menuItem : MenuItem) : Void;
-  // Class Method: Menu.buildFromTemplate(template)
-  // Menu.popup(browserWindow, [x, y])
-  // Menu.insert(pos, menuItem)
-  // Menu.items
+  function popup(browserWindow : BrowserWindos, Array<Float>) : Void;
+  function insert(pos : Int, menuItem : MenuItem) : Void;
+  var item(default, null) : Array<MenuItem>;
 }
