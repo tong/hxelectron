c1007bf4ce642aa38eb1bb24dd358f75b7af12cf
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-08-07 23:28:31 -0600

    externs for GlobalShortcut

--- src/electron/main/GlobalShortcut.hx
+++ src/electron/main/GlobalShortcut.hx
@@ -2,8 +2,8 @@ package electron.main;
 
 @:jsRequire("global-shortcut")
 extern class GlobalShortcut {
-  //globalShortcut.register(accelerator, callback)
-  //globalShortcut.isRegistered(accelerator)
-  //globalShortcut.unregister(accelerator)
-  //globalShortcut.unregisterAll()
+  static function register(accelerator : String, callback : Void -> Void) : Void;
+  static function isRegistered(accelerator : String) : Void;
+  static function unregister(accelerator : String) : Void;
+  static function unregisterAll() : Void;
 }
