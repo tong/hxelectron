eadec5df0a4b9ce702096f2473f7ce75d3dea8fc
Franco Ponticelli <franco.ponticelli@gmail.com>
2017-03-03 07:49:00 -0700

    remove comment

--- submit.sh
+++ submit.sh
@@ -4,4 +4,4 @@ neko run.n electron-api.json
 haxe doc.hxml
 rm -f electron.zip
 zip -r electron.zip src/ haxedoc.xml haxelib.json LICENSE README.md run.n -x "*/\.*"
-#haxelib submit electron.zip
+haxelib submit electron.zip
