8daada4ebaa94c0477d7994cb471b6020bb7d799
tong <tong@disktree.net>
2017-03-03 09:18:57 +0100

    Update and re-add submit.sh

---
+++ submit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+haxe run.hxml --no-traces -dce full
+neko run.n electron-api.json
+haxe doc.hxml
+rm -f electron.zip
+zip -r electron.zip src/ haxedoc.xml haxelib.json LICENSE README.md run.n -x "*/\.*"
+#haxelib submit electron.zip
