72ab465fb33e4447b220d8a60448640cd54da44b
tong <tong@disktree.net>
2018-08-11 15:18:28 +0200

    Update demo application

--- demo/src/Main.hx
+++ demo/src/Main.hx
@@ -5,20 +5,31 @@ import electron.main.BrowserWindow;
 
 class Main {
 
+	static var window : BrowserWindow;
+
+	static function createWindow() {
+		window = new BrowserWindow( { width: 720, height: 480 } );
+		window.on( closed, function() {
+			window = null;
+		});
+		window.loadFile( 'app.html' );
+		//window.webContents.openDevTools();
+	}
+
 	static function main() {
 
 		electron.CrashReporter.start({
-			companyName : "hxelectron (not a company)",
+			companyName: 'hxelectron',
 			submitURL : "https://github.com/tong/hxelectron/issues"
 		});
 
-		electron.main.App.on( 'ready', function(e) {
-			var win = new BrowserWindow( { width: 720, height: 480 } );
-			win.on( closed, function() {
-				if( Node.process.platform != 'darwin' )
-					electron.main.App.quit();
-			});
-			win.loadURL( 'file://${Node.__dirname}/app.html' );
+		electron.main.App.on( ready, function(e) {
+			createWindow();
+		});
+
+		electron.main.App.on( window_all_closed, function(e) {
+			if( Node.process.platform != 'darwin' )
+				electron.main.App.quit();
 		});
 	}
 
