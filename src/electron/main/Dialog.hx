5163380ce3ebfa5e9498b4f102e93a163c44e53a
tong <tong@disktree.net>
2020-04-14 10:47:01 +0200

    upgrade to electron v8.2.2

--- src/electron/main/Dialog.hx
+++ src/electron/main/Dialog.hx
@@ -150,7 +150,7 @@ package electron.main;
 		
 		Shows a message box, it will block the process until the message box is closed. It returns the index of the clicked button.
 		
-		The `browserWindow` argument allows the dialog to attach itself to a parent window, making it modal.
+		The `browserWindow` argument allows the dialog to attach itself to a parent window, making it modal. If `browserWindow` is not shown dialog will not be attached to it. In such case It will be displayed as independed window.
 	**/
 	static function showMessageBoxSync(?browserWindow:electron.main.BrowserWindow, options:{ /**
 		Can be `"none"`, `"info"`, `"error"`, `"question"` or `"warning"`. On Windows, `"question"` displays the same icon as `"info"`, unless you set an icon using the `"icon"` option. On macOS, both `"warning"` and `"error"` display the same warning icon.
