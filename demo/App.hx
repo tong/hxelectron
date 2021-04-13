9c002871772518a8adc8c7ffdd7909bd1e3b9262
tong <tong@disktree.net>
2021-04-13 21:09:43 +0200

    Demo cleanup

--- demo/App.hx
+++ demo/App.hx
@@ -13,35 +13,22 @@ class App {
 
     static function main() {
 
-        js.Browser.window.onload = function() {
+        window.onload = () -> {
 
             document.getElementById( 'logo-haxe' ).style.opacity = '1';
-
+            
             setText( 'system-version', process.platform +' '+ process.arch );
             setText( 'node-version', 'node '+process.version );
             setText( 'electron-version', 'electron '+process.versions['electron'] );
-
-            ChildProcess.spawn( 'haxe', ['-version'] ).stdout.on( 'data', function(buf) {
-				var version = buf.toString();
+            
+            ChildProcess.spawn( 'haxe', ['-version'] ).stdout.on( 'data', buf -> {
+                var version = buf.toString();
                 setText( 'haxe-version', 'haxe $version' );
-            });
-
-            //electron.remote.Dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] });
-            //trace(electron.remote.Dialog);
-            //trace(js.Lib.require("electron"));
-            trace(electron.renderer.Remote);
-            //trace(electron.remote.Menu);
-
-            var menu = new electron.remote.Menu();
-			menu.append(new electron.remote.MenuItem({ label: 'MenuItem11111111'} ) );
-			electron.remote.Menu.setApplicationMenu(menu);
-
-
+            }); 
+            
             /* var menu = new electron.remote.Menu();
-			menu.append(new electron.remote.MenuItem({ label: 'MenuItem1'} ) );
-			electron.remote.Menu.setApplicationMenu(menu);
-            //menu.popup({ window: electron.renderer.Remote.getCurrentWindow() });
-            js.Lib.require("electron.remote.menu"); */
+			menu.append(new electron.remote.MenuItem({ label: 'XXX'}));
+			electron.remote.Menu.setApplicationMenu(menu); */
         }
     }
 
--- demo/Main.hx
+++ demo/Main.hx
@@ -1,19 +1,24 @@
 
-import js.Node;
-import js.Node.__dirname;
 import electron.main.App;
 import electron.main.BrowserWindow;
+import js.Node.__dirname;
+import js.Node.process;
 
 class Main {
 
 	static function main() {
 
-		electron.main.App.on( ready, function(e) {
+		Sys.println( process.platform +' '+ process.arch );
+		Sys.println( 'node '+process.version );
+		Sys.println( 'electron '+process.versions['electron'] );
+
+		electron.main.App.on( ready, e -> {
 
 			var win = new BrowserWindow( {
 				width: 720, height: 480,
 				webPreferences: {
 					nodeIntegration: true,
+					contextIsolation: false,
 					enableRemoteModule: true
 				}
 			} );
@@ -33,8 +38,8 @@ class Main {
 			// electron.main.Menu.setApplicationMenu(menu);
 		});
 
-		electron.main.App.on( window_all_closed, function(e) {
-			if( Node.process.platform != 'darwin' ) electron.main.App.quit();
+		electron.main.App.on( window_all_closed, e -> {
+			if( process.platform != 'darwin' ) electron.main.App.quit();
 		});
 	}
 
