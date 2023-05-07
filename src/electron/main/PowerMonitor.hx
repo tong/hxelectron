258c2a71a531fa05fb86a53dfa0bfca7cf60004e
tong <tong@disktree.net>
2023-05-07 08:29:57 +0200

    Upgrade to electron v24.2.0

--- src/electron/main/PowerMonitor.hx
+++ src/electron/main/PowerMonitor.hx
@@ -13,7 +13,7 @@ package electron.main;
 	**/
 	static var onBatteryPower : Bool;
 	/**
-		The system's current state. Can be `active`, `idle`, `locked` or `unknown`.
+		The system's current idle state. Can be `active`, `idle`, `locked` or `unknown`.
 		
 		Calculate the system idle state. `idleThreshold` is the amount of time (in seconds) before considered idle.  `locked` is available on supported systems only.
 	**/
@@ -24,6 +24,10 @@ package electron.main;
 		Calculate system idle time in seconds.
 	**/
 	static function getSystemIdleTime():Int;
+	/**
+		The system's current thermal state. Can be `unknown`, `nominal`, `fair`, `serious`, or `critical`.
+	**/
+	static function getCurrentThermalState():String;
 	/**
 		Whether the system is on battery power.
 		
@@ -48,6 +52,18 @@ package electron.main;
 		Emitted when system changes to battery power.
 	**/
 	var on_battery : electron.main.PowerMonitorEvent<Void -> Void> = "on-battery";
+	/**
+		Emitted when the thermal state of the system changes. Notification of a change in the thermal status of the system, such as entering a critical temperature range. Depending on the severity, the system might take steps to reduce said temperature, for example, throttling the CPU or switching on the fans if available.
+		
+		Apps may react to the new state by reducing expensive computing tasks (e.g. video encoding), or notifying the user. The same state might be received repeatedly.
+		
+		See https://developer.apple.com/library/archive/documentation/Performance/Conceptual/power_efficiency_guidelines_osx/RespondToThermalStateChanges.html
+	**/
+	var thermal_state_change : electron.main.PowerMonitorEvent<Void -> Void> = "thermal-state-change";
+	/**
+		Notification of a change in the operating system's advertised speed limit for CPUs, in percent. Values below 100 indicate that the system is impairing processing power due to thermal management.
+	**/
+	var speed_limit_change : electron.main.PowerMonitorEvent<Void -> Void> = "speed-limit-change";
 	/**
 		Emitted when the system is about to reboot or shut down. If the event handler invokes `e.preventDefault()`, Electron will attempt to delay system shutdown in order for the app to exit cleanly. If `e.preventDefault()` is called, the app should exit as soon as possible by calling something like `app.quit()`.
 	**/
