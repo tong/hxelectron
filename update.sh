c332a5c02ce7462e4fe2202ac8af723773f269b8
tong <tong@disktree.net>
2024-10-05 15:55:56 +0200

    Update script formatting

--- update.sh
+++ update.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-    next=$(gh release list --repo electron/electron --limit 1 --exclude-drafts | awk '{print $2}')
+  next=$(gh release list --repo electron/electron --limit 1 --exclude-drafts | awk '{print $2}')
 else
-    next=$1
+  next=$1
 fi
 
 #current=$(git describe --tags --abbrev=0)
-current=$(jq  -r '.version' haxelib.json)
+current=$(jq -r '.version' haxelib.json)
 prerelease_version=$(echo $next | awk -F'-' '{print $2}')
 
 if [[ -n "$prerelease_version" ]]; then
-    echo "Aborting update since latest version is a pre-release ($next)"
-    exit 1
+  echo "Aborting update since latest version is a pre-release ($next)"
+  exit 1
 fi
 
 if [ "$current" = "$next" ]; then
-    echo "No new version available"
-    exit 1
+  echo "No new version available"
+  exit 1
 fi
 
 echo "Updating from $current -> $next"
@@ -29,10 +29,10 @@ haxe api.hxml
 echo "Building haxedoc.xml"
 haxe haxedoc.hxml
 echo "Updating haxelib.json"
-cat <<< "$(jq --arg var "$(echo "$next" | cut -c2-)" '.version = $var' haxelib.json)" > tmpfile && mv tmpfile haxelib.json
+cat <<<"$(jq --arg var "$(echo "$next" | cut -c2-)" '.version = $var' haxelib.json)" >tmpfile && mv tmpfile haxelib.json
 echo "Updating demo/package.json"
 cd demo || exit 1
-cat <<< "$(jq --arg var "$next" '.devDependencies.electron = $var' package.json)" > tmpfile && mv tmpfile package.json
+cat <<<"$(jq --arg var "$next" '.devDependencies.electron = $var' package.json)" >tmpfile && mv tmpfile package.json
 npm install
 cd - || exit 1
 exit 0
